86a6bc6c99d3bad1efab53954732b0eac1fadbac
Samet Çağrı Aktepe <sametcagriaktepe@gmail.com>
2023-07-26 13:10:08 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const DestiniApp());
+}
+
+class DestiniApp extends StatelessWidget {
+  const DestiniApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: StoryPage(),
+    );
+  }
+}
+
+class StoryPage extends StatefulWidget {
+  const StoryPage({super.key});
+
+  @override
+  State<StoryPage> createState() => _StoryPageState();
+}
+
+class _StoryPageState extends State<StoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Destini'),
+      ),
+      body: Center(
+        child: Text('Story Page'),
+      ),
+    );
+  }
+}
+
+
+
